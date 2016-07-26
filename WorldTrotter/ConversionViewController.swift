4fb6bdb8f3a88b1a6594cff4c4ca464f744a9635
angel.gomez <angelegomezsd@gmail.com>
2016-07-25 20:38:06 -0500

    Text Input and Delegation

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Angel Gomez on 7/25/16.
+//  Copyright © 2016 Angel Gomez Salazar. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    var farenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = farenheitValue {
+            return (value  - 32) * (5/9)
+        }
+        else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeparator = textField.text?.rangeOfString(".")
+        let replacementTextHasDecimalSeparator = string.rangeOfString(".")
+        
+        if existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    let numberFormatter: NSNumberFormatter = {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+        
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        if let text = textField.text, value = Double(text) {
+            farenheitValue = value
+        }
+        else {
+            farenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+    
+}
